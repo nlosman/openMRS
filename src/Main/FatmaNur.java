e05d1d9db2b8c3d02dc83c2a14e3bfb50d92daf9
111333444 <coban.fatmanur22@gmail.com>
2023-09-08 20:20:15 +0200

    FatmaNur Logout testine basladi

---
+++ src/Main/FatmaNur.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class FatmaNur extends BaseDriver {
+
+    @Test
+    public void Test(){
+
+
+
+
+    }
+}
