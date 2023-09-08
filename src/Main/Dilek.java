499aaa6b38f84c1dc8db07d06bf648649666dad8
Dilek1979 <dilekatilla79@outlook.com>
2023-09-09 00:03:56 +0200

    Dilek class acildi

---
+++ src/Main/Dilek.java
@@ -0,0 +1,27 @@
+package Main;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class Dilek extends BaseDriver {
+    @Test
+    public void Test1 (){
+      driver.get("https://demo.openmrs.org/");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
